f4baa6a7a944ca25c030ce31ba81f35c7aa9d0b9
bk973 <benkalungi256@gmail.com>
2022-08-10 13:24:50 +0300

    chore: Adds example workflow to repo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test( 'renders welcome message', () =>
+{
+  render( <App /> );
+
+  const message = screen.getByText( 'Welcome' );
+  expect( message ).toBeInTheDocument();
+} );
